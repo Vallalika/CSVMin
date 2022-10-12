1a1eaf5a58834b275944666fefdac39b44c39959
Valerie Bayard <valerie.bayard@gmail.com>
2022-10-12 15:41:57 +0100

    added external files, modified .gitignore and added test for coldestHourInFile method

--- src/test/java/CSVMinTest.java
+++ src/test/java/CSVMinTest.java
@@ -1,2 +1,24 @@
+import edu.duke.FileResource;
+import org.apache.commons.csv.CSVParser;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+
 public class CSVMinTest {
+
+    CSVMin csvMin;
+
+    @Before
+    public void before() {
+        csvMin = new CSVMin();
+    }
+
+    @Test
+    public void coldestHourInFileTest() {
+        FileResource fr = new FileResource("weather-2014-01-08.csv");
+        CSVParser parser = fr.getCSVParser();
+        assertEquals(15.9,csvMin.coldestHourInFile(parser).getRecord("TemperatureF"),0.00);
+    }
 }
